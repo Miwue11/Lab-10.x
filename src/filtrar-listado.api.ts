df9d881aa2af5e3ebf70109f9b97d9c107de8144
Miguel Lopez Rodriguez <hr2miguellopez@gmail.com>
2025-04-08 09:55:40 +0200

    cat

--- src/filtrar-listado.api.ts
+++ src/filtrar-listado.api.ts
@@ -5,7 +5,9 @@ export const filtrarPersonajes = async (
   nombre: string
 ): Promise<Personaje[]> => {
   try {
-    const { data } = await axios.get("http://localhost:3000/personajes");
+    const { data } = await axios.get(
+      `http://localhost:3000/personajes?nombre_like=${nombre}`
+    );
     return data.filter((personaje: Personaje) =>
       personaje.nombre.toLowerCase().includes(nombre.toLowerCase())
     );
